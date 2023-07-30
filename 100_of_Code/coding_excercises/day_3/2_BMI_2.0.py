d89a5d8bf710f62c7169dbc3997a7863cc2af55b
antonomag <40492987+antonomag@users.noreply.github.com>
2023-07-30 12:08:08 +0200

    Update and rename 2_BMI_2.0 to 2_BMI_2.0.py

---
+++ 100_of_Code/coding_excercises/day_3/2_BMI_2.0.py
@@ -0,0 +1,17 @@
+# 🚨 Don't change the code below 👇
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+bmi =  round(weight/height ** 2)
+if bmi >= 35:
+    print(f"Your BMI is {bmi}, you are clinically obese.")
+elif bmi >= 30:
+    print(f"Your BMI is {bmi}, you are obese.")
+elif bmi >= 25:
+    print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif bmi >= 18.5:
+    print(f"Your BMI is {bmi}, you have a normal weight.")
+else:
+    print(f"Your BMI is {bmi}, you are underweight.")
