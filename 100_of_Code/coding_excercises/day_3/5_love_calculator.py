6aa6d9c202c417bc53a29b403d91a43fae525aef
antonomag <40492987+antonomag@users.noreply.github.com>
2023-08-05 13:31:53 +0200

    Update 5_love_calculator.py

--- 100_of_Code/coding_excercises/day_3/5_love_calculator.py
+++ 100_of_Code/coding_excercises/day_3/5_love_calculator.py
@@ -1,313 +1,42 @@
-
-Exercise 5 - Love Calculator (Antonio Magaña Macías - Solution Workspace)
-
-💪 This is a Difficult Challenge 💪
-Instructions
-
-You are going to write a program that tests the compatibility between two people.
-
-To work out the love score between two people:
-
-    Take both people's names and check for the number of times the letters in the word TRUE occurs. 
-
-    Then check for the number of times the letters in the word LOVE occurs. 
-
-    Then combine these numbers to make a 2 digit number.
-
-For Love Scores less than 10 or greater than 90, the message should be:
-
-"Your score is **x**, you go together like coke and mentos."
-
-For Love Scores between 40 and 50, the message should be:
-
-"Your score is **y**, you are alright together."
-
-Otherwise, the message will just be their score. e.g.:
-
-"Your score is **z**."
-
-e.g.
-
-name1 = "Angela Yu"
-name2 = "Jack Bauer"
-
-T occurs 0 times
-
-R occurs 1 time
-
-U occurs 2 times
-
-E occurs 2 times
-
-Total = 5
-
-L occurs 1 time
-
-O occurs 0 times
-
-V occurs 0 times
-
-E occurs 2 times
-
-Total = 3
-
-Love Score = 53
-
-Print: "Your score is 53."
-Example Input 1
-
-name1 = "Kanye West"
-name2 = "Kim Kardashian"
-
-Example Output 1
-
-Your score is 42, you are alright together.
-
-Example Input 2
-
-name1 = "Brad Pitt"
-name2 = "Jennifer Aniston"
-
-Example Output 2
-
-Your score is 73.
-
-e.g. When you hit run, this is what should happen:
-
-The testing code will check for print output that is formatted like one of the lines below:
-
-"Your score is 47, you are alright together."
-"Your score is 125, you go together like coke and mentos."
-"Your score is 54."
-
-Score Comparison
-
-Not sure you're getting the correct score for the exercise? Use this table to check your code's score against mine.
-Name 1	Name 2	Score
-Catherine Zeta-Jones     	Michael Douglas    	99
-Brad Pitt	Jennifer Aniston	73
-Prince William	Kate Middleton	67
-Angela Yu	Jack Bauer	53
-Kanye West	Kim Kardashian	42
-Beyonce	Jay-Z	23
-John Lennon	Yoko Ono	18
-Hint
-
-    The lower() function changes all the letters in a string to lowercase.
-
-https://stackoverflow.com/questions/6797984/how-do-i-lowercase-a-string-in-python
-
-    The count() function will give you the number of times a letter occurs in a string.
-
-https://stackoverflow.com/questions/1155617/count-the-number-occurrences-of-a-character-in-a-string
-Test Your Code
-
-Check your code is doing what it is supposed to. When you're happy with your code, click submit to check your solution.
-Solution
-
-https://repl.it/@appbrewery/day-3-5-solution
-Online (1)
-Antonio Magaña Macías
-30
-31
-32
-33
-34
-35
-36
-37
-38
-39
-40
-41
-42
-43
-44
-45
-46
-47
-48
-49
-50
-51
-20 of 21
-Day 3 / Exercise 5 - Love Calculator
-100 Days of Code: The Complete Python Pro Bootcamp
-Day 1  
-Exercise 1 - Printing   Submitted
-No due date
-Attempts: 1 Most Recent Score: 100%
-
-Exercise 2 - Debugging Practice   Submitted
-No due date
-Attempts: 3 Most Recent Score: 100%
-
-Exercise 3 - Input Function   Submitted
-No due date
-Attempts: 3 Most Recent Score: 100%
-
-Exercise 4 - Variables   Submitted
-No due date
-Attempts: 2 Most Recent Score: 100%
-
-Day 1 Project: Band Name Generator   Submitted
-No due date
-Attempts: 1 Most Recent Score: 0%
-
-Day 2  
-Exercise 1 - Data Types   Submitted
-No due date
-Attempts: 2 Most Recent Score: 100%
-
-Exercise 2 - BMI Calculator   Submitted
-No due date
-Attempts: 2 Most Recent Score: 100%
-
-Exercise 3 - Life in Weeks   Submitted
-No due date
-Attempts: 1 Most Recent Score: 100%
-
-Day 2 Project: Tip Calculator   Open
-No due date
-Attempts: 0 No Submissions
-
-Day 3  
-Exercise 1 - Odd or Even   Submitted
-No due date
-Attempts: 1 Most Recent Score: 100%
-
-Exercise 2 - BMI 2.0   Submitted
-No due date
-Attempts: 4 Most Recent Score: 100%
-
-Exercise 3 - Leap Year   Submitted
-No due date
-Attempts: 2 Most Recent Score: 100%
-
-Exercise 4 - Pizza Order Practice   Submitted
-No due date
-Attempts: 1 Most Recent Score: 100%
-
-Exercise 5 - Love Calculator   In Progress
-No due date
-Attempts: 0 No Submissions
-
-Day 3 Project: Treasure Island   Open
-No due date
-Attempts: 0 No Submissions
-
-Day 4  
-Exercise 1 - Heads or Tails   Open
-No due date
-Attempts: 0 No Submissions
-
-Exercise 2 - Banker Roulette   Open
-No due date
-Attempts: 0 No Submissions
-
-Exercise 3 - Treasure Map   Open
-No due date
-Attempts: 0 No Submissions
-
-Day 4 Project: Rock Paper Scissors   Open
-No due date
-Attempts: 0 No Submissions
-
-Day 5  
-Exercise 1 - Average Height   Open
-No due date
-Attempts: 0 No Submissions
-
-Exercise 2 - High Score   Open
-No due date
-Attempts: 0 No Submissions
-
-Exercise 3 - Adding Even Numbers   Open
-No due date
-Attempts: 0 No Submissions
-
-Exercise 4 - FizzBuzz   Open
-No due date
-Attempts: 0 No Submissions
-
-Day 5 Project: Create a Password Generator   Open
-No due date
-Attempts: 0 No Submissions
-
-Day 8  
-Exercise 1 - Paint Area Calculator   Open
-No due date
-Attempts: 0 No Submissions
-
-Exercise 2 - Prime Numbers   Open
-No due date
-Attempts: 0 No Submissions
-
-Day 9  
-Exercise 1 - Grading Program   Open
-No due date
-Attempts: 0 No Submissions
-
-Exercise 2 - Dictionary in List   Open
-No due date
-Attempts: 0 No Submissions
-
-Day 10  
-Exercise 1 - Days in Month   Open
-No due date
-Attempts: 0 No Submissions
-
-Day 13  
-Exercise 1 - Debugging Odd or Even   Open
-No due date
-Attempts: 0 No Submissions
-
-Exercise 2 - Debugging Leap Year   Open
-No due date
-Attempts: 0 No Submissions
-
-Exercise 3 - Debugging FizzBuzz   Open
-No due date
-Attempts: 0 No Submissions
-
-Day 26  
-Exercise 1 - Squaring Numbers   Open
-No due date
-Attempts: 0 No Submissions
-
-Exercise 2 - Filtering Even Numbers   Open
-No due date
-Attempts: 0 No Submissions
-
-Exercise 3 - Data Overlap   Open
-No due date
-Attempts: 0 No Submissions
-
-Exercise 4 - Dictionary Comprehension 1   Open
-No due date
-Attempts: 0 No Submissions
-
-Exercise 5 - Dictionary Comprehension 2   Open
-No due date
-Attempts: 0 No Submissions
-
-Day 30  
-Exercise 1 - IndexError Handling   Open
-No due date
-Attempts: 0 No Submissions
-
-Exercise 2 - KeyError Handling   Open
-No due date
-Attempts: 0 No Submissions
-
-Day 54  
-Exercise 1 - Create Your Own Python Decorator   Open
-No due date
-Attempts: 0 No Submissions
-
-Day 55  
-Exercise 1 - Advanced Decorators   Open
-No due date
-Attempts: 0 No Submissions
-
-print(score1), 20 of 21 found for '1', at 21:12
+# 🚨 Don't change the code below 👇
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+name = (name1+ name2).lower()
+
+# name_t = name.count('t')
+# name_r = name.count('r')
+# name_u = name.count('u')
+# name_e = name.count('e')
+# name_l = name.count('l')
+# name_o = name.count('o')
+# name_v = name.count('v')
+
+# score1 = str(name_t + name_r + name_u + name_e)
+# score2 = str(name_l + name_o + name_v + name_e)
+# score1 = sum(true)
+# score2 = sum(love)
+#print('Your score is ' + true + love + '.')
+
+#solution with lists and sum()
+t = name.count('t')
+r = name.count('r')
+u = name.count('u')
+e = name.count('e')
+l = name.count('l')
+o = name.count('o')
+v = name.count('v')
+
+true = (t,r,u,e)
+love = (l,o,v,e)
+score = int(str(sum(true))+str(sum(love)))
+
+if score < 10 or score > 90:
+    print(f'Your score is {score}, you go together like coke and mentos.')
+elif score >= 40 and score <= 50:
+    print(f'Your score is {score}, you are alright together.')
+else:
+    print(f'Your score is {score}.')
